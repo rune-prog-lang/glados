f8b5c66e57263a5ab8710fbca43be123bbb330f4
shmul95 <samuel.gross@epitech.eu>
2025-11-26 12:04:03 +0100

    [ADD] mutliple scope inside function

--- Rune/src/Rune/Semantics/Vars.hs
+++ Rune/src/Rune/Semantics/Vars.hs
@@ -16,23 +16,37 @@ verifDefs (DefFunction _ params _ body) = verifScope (Set.fromList (map paramNam
 verifDefs (DefOverride _ params _ body) = verifScope (Set.fromList (map paramName params)) body
 verifDefs _ = Nothing
 
--- visitStatement (StmtVarDecl name typeDecl expr) = visitVarDecl name typeDecl expr
--- visitStatement (StmtReturn expr) = visitReturn expr
--- visitStatement (StmtIf cond thenB elseB) = visitIf cond thenB elseB
--- visitStatement (StmtFor var start end body) = visitFor var start end body
--- visitStatement (StmtForEach var iterable body) = visitForEach var iterable body
--- visitStatement (StmtExpr expr) = void $ visitExpression expr
-verifScope :: ScopeStack -> Block -> Maybe String
-verifScope scope ((StmtVarDecl name _ expr):stmts) = 
-    verifExpr scope expr <> verifScope (Set.insert name scope) stmts
-verifScope _ _ = Nothing
 
--- visitExpression (ExprBinary _ l r) = visitExpression l >> visitExpression r
--- visitExpression (ExprUnary _ val) = visitExpression val
--- visitExpression (ExprCall _ args) = mapM_ visitExpression args
--- visitExpression (ExprStructInit _ fields) = mapM_ (visitExpression . snd) fields
--- visitExpression (ExprAccess target _) = visitExpression target
--- visitExpression _ = return ()
+verifScope :: ScopeStack -> Block -> Maybe String
+verifScope s ((StmtVarDecl name _ expr):stmts)
+    = let s' = (Set.insert name s)
+    in  verifExpr s expr
+    <>  verifScope s' stmts
+verifScope s ((StmtReturn expr):stmts)
+    =   maybe Nothing (verifExpr s) expr
+    <>  verifScope s stmts
+verifScope s ((StmtIf cond thenB elseB):stmts)
+    =   verifExpr s cond
+    <>  verifScope s thenB
+    <>  maybe Nothing (verifScope s) elseB
+    <>  verifScope s stmts
+verifScope s ((StmtFor var start end body):stmts)
+    = let s' = Set.insert var s
+    in  verifExpr s' start
+    <>  verifExpr s' end
+    <>  verifScope s' body
+    <>  verifScope s stmts
+verifScope s ((StmtForEach var iterable body):stmts)
+    = let s' = Set.insert var s
+    in  verifExpr s' iterable
+    <>  verifScope s' body
+    <>  verifScope s stmts
+verifScope s ((StmtExpr expr):stmts) 
+    =   verifExpr s expr
+    <>  verifScope s stmts
+verifScope _ [] = Nothing
+
+
 verifExpr :: ScopeStack -> Expression -> Maybe String
 verifExpr s (ExprBinary _ l r) = (verifExpr s l) <> (verifExpr s r)
 verifExpr s (ExprCall _ args) = foldMap (verifExpr s) args
@@ -42,6 +56,6 @@ verifExpr s (ExprUnary _ val) = verifExpr s val
 verifExpr s (ExprVar var) =
     case Set.member var s of
         True -> Nothing
-        False -> Just $ "\n\t" ++ var ++ " : doesn't exist in the scope"
+        False -> Just $ "\n\t" ++ var ++ " : var doesn't exist in the scope"
 verifExpr _ _ = Nothing
 
