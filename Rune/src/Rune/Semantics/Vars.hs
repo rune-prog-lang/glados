521b95a612dabeb96d8e2ae4a01f82556b7516d1
shmul95 <samuel.gross@epitech.eu>
2025-11-26 16:54:21 +0100

    [ADD]: integrating function stack to the variable stack for type inference

--- Rune/src/Rune/Semantics/Vars.hs
+++ Rune/src/Rune/Semantics/Vars.hs
@@ -4,26 +4,29 @@ import Rune.AST.Nodes
 import Data.HashMap.Strict (HashMap)
 import Data.Maybe (fromMaybe)
 import qualified Data.HashMap.Strict as HM
+import Rune.Semantics.Func (findFunc, FuncStack)
 
 type VarStack = HashMap String Type
+type Stack = (FuncStack, VarStack)
 
 -- if Nothing everything good else Error message
 verifVars :: Program -> Maybe String
-verifVars (Program _ defs) = foldMap verifDefs defs
+verifVars (Program n defs) =
+    foldMap (verifDefs (findFunc (Program n defs))) defs
 
-verifDefs :: TopLevelDef -> Maybe String
-verifDefs (DefFunction _ params _ body) = verifScope (HM.fromList (map (\p -> (paramName p, paramType p)) params)) body
-verifDefs (DefOverride _ params _ body) = verifScope (HM.fromList (map (\p -> (paramName p, paramType p)) params)) body
--- verifDefs (DefStruct _ attr _) = -- when handling struct each var of type struct can expand to var.attr
-verifDefs _ = Nothing
+verifDefs :: FuncStack -> TopLevelDef -> Maybe String
+verifDefs fs (DefFunction _ params _ body) = verifScope (fs, HM.fromList (map (\p -> (paramName p, paramType p)) params)) body
+verifDefs fs (DefOverride _ params _ body) = verifScope (fs, HM.fromList (map (\p -> (paramName p, paramType p)) params)) body
+-- verifDefs fs (DefStruct _ attr _) = undefined -- when there will be type and all
+verifDefs _ _ = Nothing
 
 
-verifScope :: VarStack -> Block -> Maybe String
+verifScope :: Stack -> Block -> Maybe String
 -- name n, type t, expr e
-verifScope s ((StmtVarDecl n t e):stmts)
-    = let s' = (HM.insert n (fromMaybe TypeAny t) s)
-    in  verifExpr s e
-    <>  verifScope s' stmts
+verifScope (fs, vs) ((StmtVarDecl n t e):stmts)
+    = let vs' = (HM.insert n (fromMaybe TypeAny t) vs)
+    in  verifExpr (fs, vs) e
+    <>  verifScope (fs, vs') stmts
 verifScope s ((StmtReturn e):stmts)
     =   maybe Nothing (verifExpr s) e
     <>  verifScope s stmts
@@ -32,31 +35,31 @@ verifScope s ((StmtIf cond thenA elseB):stmts)
     <>  verifScope s thenA
     <>  maybe Nothing (verifScope s) elseB
     <>  verifScope s stmts
-verifScope s ((StmtFor var start end body):stmts)
-    = let s' = HM.insert var TypeAny s
-    in  verifExpr s' start
-    <>  verifExpr s' end
-    <>  verifScope s' body
-    <>  verifScope s stmts
-verifScope s ((StmtForEach var iterable body):stmts)
-    = let s' = HM.insert var TypeAny s
-    in  verifExpr s' iterable
-    <>  verifScope s' body
-    <>  verifScope s stmts
+verifScope (fs, vs) ((StmtFor var start end body):stmts)
+    = let vs' = HM.insert var TypeAny vs
+    in  verifExpr (fs, vs') start
+    <>  verifExpr (fs, vs') end
+    <>  verifScope (fs, vs') body
+    <>  verifScope (fs, vs) stmts
+verifScope (fs, vs) ((StmtForEach var iterable body):stmts)
+    = let vs' = HM.insert var TypeAny vs
+    in  verifExpr (fs, vs') iterable
+    <>  verifScope (fs, vs') body
+    <>  verifScope (fs, vs) stmts
 verifScope s ((StmtExpr e):stmts) 
     =   verifExpr s e
     <>  verifScope s stmts
 verifScope _ [] = Nothing
 
 
-verifExpr :: VarStack -> Expression -> Maybe String
+verifExpr :: Stack -> Expression -> Maybe String
 verifExpr s (ExprBinary _ l r) = (verifExpr s l) <> (verifExpr s r)
 verifExpr s (ExprCall _ args) = foldMap (verifExpr s) args
 verifExpr s (ExprStructInit _ fields) = foldMap (verifExpr s . snd) fields
 verifExpr s (ExprAccess target _) = verifExpr s target
 verifExpr s (ExprUnary _ val) = verifExpr s val
 verifExpr s (ExprVar var) =
-    case HM.member var s of
+    case HM.member var (snd s) of
         True -> Nothing
         False -> Just $ "\n\t" ++ var ++ " : var doesn't exist in the scope"
 verifExpr _ _ = Nothing
