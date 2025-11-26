a1a0f2e7f25e2a99e7be2de3be0a85dd0caa10e5
shmul95 <samuel.gross@epitech.eu>
2025-11-26 14:29:45 +0100

    [ADD] using HMap instead of Set

--- Rune/src/Rune/Semantics/Vars.hs
+++ Rune/src/Rune/Semantics/Vars.hs
@@ -1,48 +1,50 @@
 module Rune.Semantics.Vars ( verifVars ) where
 
--- import Rune.AST.Visitor (RuneVisitor (..))
 import Rune.AST.Nodes
-import Data.Set (Set)
-import qualified Data.Set as Set
+import Data.HashMap.Strict (HashMap)
+import Data.Maybe (fromMaybe)
+import qualified Data.HashMap.Strict as HM
 
-type ScopeStack = Set String
+type ScopeStack = HashMap String Type
 
 -- if Nothing everything good else Error message
 verifVars :: Program -> Maybe String
 verifVars (Program _ defs) = foldMap verifDefs defs
 
 verifDefs :: TopLevelDef -> Maybe String
-verifDefs (DefFunction _ params _ body) = verifScope (Set.fromList (map paramName params)) body
-verifDefs (DefOverride _ params _ body) = verifScope (Set.fromList (map paramName params)) body
+verifDefs (DefFunction _ params _ body) = verifScope (HM.fromList (map (\p -> (paramName p, paramType p)) params)) body
+verifDefs (DefOverride _ params _ body) = verifScope (HM.fromList (map (\p -> (paramName p, paramType p)) params)) body
+-- verifDefs (DefStruct _ attr _) = -- when handling struct each var of type struct can expand to var.attr
 verifDefs _ = Nothing
 
 
 verifScope :: ScopeStack -> Block -> Maybe String
-verifScope s ((StmtVarDecl name _ expr):stmts)
-    = let s' = (Set.insert name s)
-    in  verifExpr s expr
+-- name n, type t, expr e
+verifScope s ((StmtVarDecl n t e):stmts)
+    = let s' = (HM.insert n (fromMaybe TypeAny t) s)
+    in  verifExpr s e
     <>  verifScope s' stmts
-verifScope s ((StmtReturn expr):stmts)
-    =   maybe Nothing (verifExpr s) expr
+verifScope s ((StmtReturn e):stmts)
+    =   maybe Nothing (verifExpr s) e
     <>  verifScope s stmts
-verifScope s ((StmtIf cond thenB elseB):stmts)
+verifScope s ((StmtIf cond thenA elseB):stmts)
     =   verifExpr s cond
-    <>  verifScope s thenB
+    <>  verifScope s thenA
     <>  maybe Nothing (verifScope s) elseB
     <>  verifScope s stmts
 verifScope s ((StmtFor var start end body):stmts)
-    = let s' = Set.insert var s
+    = let s' = HM.insert var TypeAny s
     in  verifExpr s' start
     <>  verifExpr s' end
     <>  verifScope s' body
     <>  verifScope s stmts
 verifScope s ((StmtForEach var iterable body):stmts)
-    = let s' = Set.insert var s
+    = let s' = HM.insert var TypeAny s
     in  verifExpr s' iterable
     <>  verifScope s' body
     <>  verifScope s stmts
-verifScope s ((StmtExpr expr):stmts) 
-    =   verifExpr s expr
+verifScope s ((StmtExpr e):stmts) 
+    =   verifExpr s e
     <>  verifScope s stmts
 verifScope _ [] = Nothing
 
@@ -54,8 +56,7 @@ verifExpr s (ExprStructInit _ fields) = foldMap (verifExpr s . snd) fields
 verifExpr s (ExprAccess target _) = verifExpr s target
 verifExpr s (ExprUnary _ val) = verifExpr s val
 verifExpr s (ExprVar var) =
-    case Set.member var s of
+    case HM.member var s of
         True -> Nothing
         False -> Just $ "\n\t" ++ var ++ " : var doesn't exist in the scope"
 verifExpr _ _ = Nothing
-
