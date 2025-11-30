0120074d6bcf93d75e225e5caad7803fb30fd313
Leorevoir <leo.quinzler@epitech.eu>
2025-11-30 19:10:51 +0100

    [CI/CD]: add struct CI//CD

--- Rune/test/IR/IRSpec.hs
+++ Rune/test/IR/IRSpec.hs
@@ -19,7 +19,8 @@ irTests =
     [ testCase "IR Test Programs" irTestShowString,
       testCase "IR For To Loop" irTestForTo,
       testCase "IR Loop Control Statements" irTestLoopControl,
-      testCase "IR Conditional Statements" irTestConditional
+      testCase "IR Conditional Statements" irTestConditional,
+      testCase "IR Structs" irTestStruct
     ]
 
 --
@@ -198,4 +199,91 @@ irTestConditional = do
             ".L.end1:",
             "    RET"
           ]
+  runIR program @?= expected
+
+irTestStruct :: IO ()
+irTestStruct = do
+  let program =
+        unlines
+          [ "struct Vec2f",
+            "{",
+            "    x: f32;",
+            "    y: f32;",
+            "",
+            "    def add(self, other: Vec2f) -> Vec2f",
+            "    {",
+            "        Vec2f {",
+            "            x: self.x + other.x,",
+            "            y: self.y + other.y",
+            "        }",
+            "    }",
+            "",
+            "}",
+            "",
+            "override def show(v: Vec2f) -> null",
+            "{",
+            "    show(\"Vec2f(x: \");",
+            "    show(v.x);",
+            "    show(\", y: \");",
+            "    show(v.y);",
+            "}",
+            "",
+            "def main() -> null",
+            "{",
+            "    a = Vec2f { x: 1.0, y: 2.0 };",
+            "    b = Vec2f { x: 3.0, y: 4.0 };",
+            "    c = a.add(b);",
+            "",
+            "    show(c);",
+            "}"
+          ]
+
+  let expected =
+        unlines
+          [ "PROGRAM test:",
+            "GLOBAL str_show_Vec2f0: string = \"Vec2f(x: \\0\"",
+            "GLOBAL str_show_Vec2f1: string = \", y: \\0\"",
+            "STRUCT Vec2f { x: f32, y: f32 }",
+            "DEF Vec2f_add(p_self: *Vec2f, p_other: *Vec2f):",
+            "    ALLOC struct0: Vec2f",
+            "    f_x1: f32 = GET_FIELD p_self, \"Vec2f\", \"x\"",
+            "    f_x2: f32 = GET_FIELD p_other, \"Vec2f\", \"x\"",
+            "    t3: f32 = ADD f_x1, f_x2",
+            "    p_init4: *Vec2f = ADDR struct0",
+            "    SET_FIELD p_init4, \"Vec2f\", \"x\", t3",
+            "    f_y5: f32 = GET_FIELD p_self, \"Vec2f\", \"y\"",
+            "    f_y6: f32 = GET_FIELD p_other, \"Vec2f\", \"y\"",
+            "    t7: f32 = ADD f_y5, f_y6",
+            "    p_init8: *Vec2f = ADDR struct0",
+            "    SET_FIELD p_init8, \"Vec2f\", \"y\", t7",
+            "    RET struct0",
+            "DEF show_Vec2f(p_v: *Vec2f):",
+            "    p_ptr0: *u8 = ADDR str_show_Vec2f0",
+            "    CALL puts(p_ptr0)",
+            "    f_x1: f32 = GET_FIELD p_v, \"Vec2f\", \"x\"",
+            "    CALL printf(f_x1)",
+            "    p_ptr2: *u8 = ADDR str_show_Vec2f1",
+            "    CALL puts(p_ptr2)",
+            "    f_y3: f32 = GET_FIELD p_v, \"Vec2f\", \"y\"",
+            "    CALL printf(f_y3)",
+            "    RET",
+            "DEF main():",
+            "    ALLOC struct0: Vec2f",
+            "    p_init1: *Vec2f = ADDR struct0",
+            "    SET_FIELD p_init1, \"Vec2f\", \"x\", 1.0",
+            "    p_init2: *Vec2f = ADDR struct0",
+            "    SET_FIELD p_init2, \"Vec2f\", \"y\", 2.0",
+            "    ALLOC struct3: Vec2f",
+            "    p_init4: *Vec2f = ADDR struct3",
+            "    SET_FIELD p_init4, \"Vec2f\", \"x\", 3.0",
+            "    p_init5: *Vec2f = ADDR struct3",
+            "    SET_FIELD p_init5, \"Vec2f\", \"y\", 4.0",
+            "    p_struct0: *Vec2f = ADDR struct0",
+            "    p_struct3: *Vec2f = ADDR struct3",
+            "    t6: Vec2f = CALL Vec2f_add(p_struct0, p_struct3)",
+            "    addr_t6: *Vec2f = ADDR t6",
+            "    CALL show_Vec2f(addr_t6)",
+            "    RET"
+          ]
+
   runIR program @?= expected
