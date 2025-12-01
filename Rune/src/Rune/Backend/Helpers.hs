66f81b1b092f4963bea7a6b534c2aa248c224854
Leorevoir <leo.quinzler@epitech.eu>
2025-12-01 22:20:42 +0100

    [FIX]: helpers

--- Rune/src/Rune/Backend/Helpers.hs
+++ Rune/src/Rune/Backend/Helpers.hs
@@ -20,9 +20,6 @@ import Rune.IR.Nodes (IRFunction (..), IRInstruction (..), IRTopLevel (..), IRTy
 emit :: Int -> String -> String
 emit lvl s = replicate (lvl * 4) ' ' ++ s
 
-escapeString :: String -> String
-escapeString s = intercalate "," (map escapeChar s)
-
 collectTopLevels :: [IRTopLevel] -> ([Extern], [GlobalString], [Function])
 collectTopLevels tls =
   let (es, gs, fs) = foldr collectTopLevel ([], [], []) tls
@@ -38,24 +35,15 @@ calculateStackMap func =
       rbpOffsetsMap = Map.map (\offset -> -(totalUsedSize - offset)) offsetsMap
    in (rbpOffsetsMap, totalSize)
 
+escapeString :: String -> String
+escapeString = intercalate "," . encodeCharacter
+
 --
 -- private
 --
 
 alignUp :: Int -> Int -> Int
 alignUp x n = (x + n - 1) `div` n * n
-
-escapeChar :: Char -> String
-escapeChar '\0' = "0"
-escapeChar '\n' = "10"
-escapeChar '\t' = "9"
-escapeChar '\r' = "13"
-escapeChar '\\' = "'\\\\'"
-escapeChar '"' = "'\"'"
-escapeChar '\'' = "'''"
-escapeChar c
-  | c >= ' ' && c <= '~' = "'" ++ [c] ++ "'"
-  | otherwise = show (fromEnum c)
 
 collectTopLevel :: IRTopLevel -> ([Extern], [GlobalString], [Function]) -> ([Extern], [GlobalString], [Function])
 collectTopLevel (IRExtern name) (e, g, f) = (name : e, g, f)
@@ -96,3 +84,17 @@ accumulateOffset (currentOffset, accMap) (name, varType) =
       size = sizeOfIRType varType
       newOffset = alignedOffset + size
    in (newOffset, Map.insert name alignedOffset accMap)
+
+encodeCharacter :: String -> [String]
+encodeCharacter "" = []
+encodeCharacter s@(c : cs)
+  | c == '\n' = "10" : encodeCharacter cs
+  | c == '\r' = "13" : encodeCharacter cs
+  | c == '\t' = "9" : encodeCharacter cs
+  | c == '\0' = "0" : encodeCharacter cs
+  | otherwise =
+      let (printables, rest) = span isPrintable s
+       in ("\"" ++ printables ++ "\"") : encodeCharacter rest
+
+isPrintable :: Char -> Bool
+isPrintable ch = ch >= ' ' && ch <= '~'
