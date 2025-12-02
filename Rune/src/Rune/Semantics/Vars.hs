639c1405d48423385b891d0e9ccdf8d12fa5748a
shmul95 <samuel.gross@epitech.eu>
2025-12-02 11:39:34 +0100

    [REFACTO] added some pattern matching where i can

--- Rune/src/Rune/Semantics/Vars.hs
+++ Rune/src/Rune/Semantics/Vars.hs
@@ -11,8 +11,7 @@ type Stack = (FuncStack, VarStack)
 
 -- if Nothing everything good else Error message
 verifVars :: Program -> Maybe String
-verifVars (Program n defs) =
-    foldMap (verifDefs (findFunc (Program n defs))) defs
+verifVars (Program n defs) = foldMap (verifDefs (findFunc (Program n defs))) defs
 
 verifDefs :: FuncStack -> TopLevelDef -> Maybe String
 verifDefs fs (DefFunction _ params _ body) = verifScope (fs, HM.fromList (map (\p -> (paramName p, paramType p)) params)) body
@@ -24,7 +23,7 @@ verifDefs _ _ = Nothing
 verifScope :: Stack -> Block -> Maybe String
 -- name n, type t, expr e
 verifScope (fs, vs) (StmtVarDecl var t e:stmts) =
-    let expr_type   = typeOfExpr (fs, vs) e
+    let expr_type   = exprType (fs, vs) e
         (vs', err)  = assignVarType vs var $ fromMaybe expr_type t 
         multipleType= checkMultipleType var t expr_type
     in  err <> multipleType 
@@ -39,7 +38,7 @@ verifScope s (StmtIf cond thenA elseB:stmts) =
     <>  (verifScope s =<< elseB)
     <>  verifScope s stmts
 verifScope (fs, vs) (StmtFor var t (Just start) end body:stmts) =
-    let expr_type   = typeOfExpr (fs, vs) start
+    let expr_type   = exprType (fs, vs) start
         (vs', err)  = assignVarType vs var $ fromMaybe expr_type t 
         multipleType= checkMultipleType var t expr_type
     in  err <> multipleType 
@@ -56,7 +55,7 @@ verifScope (fs, vs) (StmtFor var t Nothing end body:stmts) =
     <>  verifScope (fs, vs') body
     <>  verifScope (fs, vs) stmts
 verifScope (fs, vs) (StmtForEach var t iterable body:stmts) =
-    let expr_type   = typeOfExpr (fs, vs) iterable
+    let expr_type   = exprType (fs, vs) iterable
         (vs', err)  = assignVarType vs var $ fromMaybe expr_type t 
         multipleType= checkMultipleType var t expr_type
     in  err <> multipleType 
@@ -71,7 +70,7 @@ verifScope s (StmtLoop block:stmts)
     <>  verifScope s stmts
 -- bit weird i don't know if it work like this
 verifScope (fs, vs) (StmtAssignment (ExprVar lv) rv:stmts) =
-    let (vs', err) = assignVarType vs lv $ typeOfExpr (fs, vs) rv
+    let (vs', err) = assignVarType vs lv $ exprType (fs, vs) rv
     in  err
     <>  verifExpr (fs, vs) rv
     <>  verifScope (fs, vs') stmts
@@ -94,49 +93,50 @@ verifExpr s (ExprVar var) =
         False -> Just msg
 verifExpr _ _ = Nothing
 
+--
+-- helper
+--
 
-typeOfExpr :: Stack -> Expression -> Type
-typeOfExpr _ (ExprLitInt _) = TypeI32
-typeOfExpr _ (ExprLitFloat  _) = TypeF32
-typeOfExpr _ (ExprLitString  _) = TypeString
-typeOfExpr _ (ExprLitChar _ ) = TypeU8
-typeOfExpr _ (ExprLitBool  _) = TypeBool
-typeOfExpr _ (ExprStructInit st _) = TypeCustom st
-typeOfExpr _ ExprLitNull = TypeNull
-typeOfExpr _ (ExprAccess _ _) = TypeAny -- don't know how to use struct
-typeOfExpr s (ExprBinary _ expr _) = typeOfExpr s expr -- assume both expr are of the same type
-typeOfExpr s (ExprUnary _ expr) = typeOfExpr s expr -- assume the op don't change the type
-typeOfExpr (fs, _) (ExprCall fn _) =
+exprType :: Stack -> Expression -> Type
+exprType _ (ExprLitInt _) = TypeI32
+exprType _ (ExprLitFloat  _) = TypeF32
+exprType _ (ExprLitString  _) = TypeString
+exprType _ (ExprLitChar _ ) = TypeU8
+exprType _ (ExprLitBool  _) = TypeBool
+exprType _ (ExprStructInit st _) = TypeCustom st
+exprType _ ExprLitNull = TypeNull
+exprType _ (ExprAccess _ _) = TypeAny -- don't know how to use struct
+exprType s (ExprBinary _ expr _) = exprType s expr -- assume both expr are of the same type
+exprType s (ExprUnary _ expr) = exprType s expr -- assume the op don't change the type
+exprType (fs, _) (ExprCall fn _) =
     case HM.lookup fn fs of
         Just (t, _) -> t
         Nothing -> TypeAny
-typeOfExpr (_, vs) (ExprVar name) =
+exprType (_, vs) (ExprVar name) =
     fromMaybe TypeAny (HM.lookup name vs)
 
 assignVarType :: VarStack -> String -> Type -> (VarStack, Maybe String)
+assignVarType s _ TypeAny = (s, Nothing)
 assignVarType s v t =
     let msg = "\n\tTypeOverwrite: " ++ v
           ++ " has alerady type " ++ show t
         updated = (HM.insert v t s, Nothing)
-        unchanged = (s, Nothing)
     in case HM.lookup v s of
       Nothing       -> updated
       Just TypeAny  -> updated
-      Just t' | t' == t         -> updated
-              | t' == TypeAny   -> updated
-              | t' == TypeNull  -> updated
-              | t  == TypeAny   -> unchanged
-              | otherwise       -> (s, Just msg)
+      Just TypeNull -> updated
+      Just t' | t' == t   -> updated
+              | otherwise -> (s, Just msg)
 
 checkMultipleType :: String -> Maybe Type -> Type -> Maybe String
+checkMultipleType _ _ TypeAny  = Nothing
+checkMultipleType _ _ TypeNull = Nothing
 checkMultipleType v t e_t = 
     let msg = "\n\tMultipleType: " ++ v
           ++ " is " ++ show t ++ " and "
           ++ show e_t
     in case t of
-        Nothing -> Nothing
-        Just t' | t'  == e_t      -> Nothing
-                | t'  == TypeAny  -> Nothing
-                | e_t == TypeAny  -> Nothing
-                | e_t == TypeNull -> Nothing
-                | otherwise       -> Just msg
+        Nothing      -> Nothing
+        Just TypeAny -> Nothing
+        Just t' | t' == e_t -> Nothing
+                | otherwise -> Just msg
