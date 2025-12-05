9a78acb652a4b5922e168ae4fd136d2f50cb052f
Leorevoir <leo.quinzler@epitech.eu>
2025-12-05 16:54:20 +0100

    [REFACTO]: create Call/ module for builtins specific

--- Rune/src/Rune/IR/Generator/Expression/Call/Show.hs
+++ Rune/src/Rune/IR/Generator/Expression/Call/Show.hs
@@ -1,8 +1,8 @@
 module Rune.IR.Generator.Expression.Call.Show (genShowCall) where
 
 import Rune.AST.Nodes (Expression)
-import Rune.IR.IRHelpers (genFormatString, makeLabel, newTemp, nextLabelIndex, registerCall)
-import Rune.IR.Nodes (IRGen, IRInstruction (..), IRLabel (..), IROperand (..), IRType (..))
+import Rune.IR.IRHelpers (genFormatString, registerCall)
+import Rune.IR.Nodes (IRGen, IRInstruction (..), IROperand (..), IRType (..))
 
 --
 -- callbacks to avoid circular dependencies
@@ -40,7 +40,6 @@ genShowCall genExpr arg = do
 -- def show(value: any) -> null
 -- we need to format the arguments accordingly to the input
 genShowFmtCall :: IROperand -> IRType -> IROperand -> IRGen ([IRInstruction], [IROperand])
-genShowFmtCall _ IRBool boolOp = genBoolShowCall boolOp
 genShowFmtCall originalOp typ finalOp = case getFormatSpecifier originalOp typ of
   Just fmt -> do
     (i, f) <- genFormatString fmt
@@ -64,7 +63,7 @@ getFormatSpecifier _ IRU64 = Just "%lu"
 getFormatSpecifier _ IRChar = Just "%c"
 getFormatSpecifier _ IRF32 = Just "%f"
 getFormatSpecifier _ IRF64 = Just "%lf"
-getFormatSpecifier _ IRBool = Nothing
+getFormatSpecifier _ IRBool = Just "%d" -- should be 1 ? "true" : "false"
 getFormatSpecifier _ IRNull = Just "(null)"
 getFormatSpecifier _ (IRPtr IRChar) = Just "%s"
 getFormatSpecifier _ _ = Nothing
@@ -79,51 +78,3 @@ prepareAddr (IRTemp n _) (IRPtr (IRStruct t)) =
   , IRTemp ("addr_" ++ n) (IRPtr (IRPtr (IRStruct t)))
   )
 prepareAddr op _ = ([], op)
-
---
--- boolean show helpers
---
-
-genBoolShowCall :: IROperand -> IRGen ([IRInstruction], [IROperand])
-genBoolShowCall boolOp = do
-  (convInstrs, strOp) <- genBoolToStringConversion boolOp
-  (fmtInstrs, fmtOp) <- genFormatString "%s"
-  return (convInstrs ++ fmtInstrs, [fmtOp, strOp])
-
-genBoolToStringConversion :: IROperand -> IRGen ([IRInstruction], IROperand)
-genBoolToStringConversion boolOp = do
-  (litInstrs, trueStr, falseStr) <- genBoolStringLiterals
-  resultTemp <- newTemp "bool_str" (IRPtr IRChar)
-  labels <- genBoolConversionLabels
-  let convInstrs = buildBoolConversionInstrs boolOp trueStr falseStr resultTemp labels
-  return (litInstrs ++ convInstrs, IRTemp resultTemp (IRPtr IRChar))
-
-genBoolStringLiterals :: IRGen ([IRInstruction], IROperand, IROperand)
-genBoolStringLiterals = do
-  (trueInstrs, trueOp) <- genFormatString "(true)"
-  (falseInstrs, falseOp) <- genFormatString "(false)"
-  return (trueInstrs ++ falseInstrs, trueOp, falseOp)
-
-genBoolConversionLabels :: IRGen (IRLabel, IRLabel, IRLabel)
-genBoolConversionLabels = do
-  trueIdx <- nextLabelIndex
-  falseIdx <- nextLabelIndex
-  endIdx <- nextLabelIndex
-  return
-    ( makeLabel "bool_true_" trueIdx,
-      makeLabel "bool_false_" falseIdx,
-      makeLabel "bool_end_" endIdx
-    )
-
-buildBoolConversionInstrs :: IROperand -> IROperand -> IROperand -> String -> (IRLabel, IRLabel, IRLabel) -> [IRInstruction]
-buildBoolConversionInstrs boolOp trueStr falseStr resultTemp (trueLabel, falseLabel, endLabel) =
-  [ IRJUMP_TRUE boolOp trueLabel,
-    IRJUMP falseLabel,
-    IRLABEL trueLabel,
-    IRASSIGN resultTemp trueStr (IRPtr IRChar),
-    IRJUMP endLabel,
-    IRLABEL falseLabel,
-    IRASSIGN resultTemp falseStr (IRPtr IRChar),
-    IRLABEL endLabel
-  ]
-
