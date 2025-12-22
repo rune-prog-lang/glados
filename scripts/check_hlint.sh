230373bdd51ef649d64ea048bfb1db0ebf10dc78
Leorevoir <leo.quinzler@epitech.eu>
2025-12-22 10:42:31 +0100

    ci(script): add check_hlint

---
+++ scripts/check_hlint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+SUCCESS=0
+ERROR=84
+
+if ! command -v hlint &> /dev/null; then
+    echo -e "${RED}Error: hlint is not installed.${NC}"
+    exit $ERROR
+fi
+
+hlint RuneLang
+RET=$?
+
+if [ $RET -ne 0 ]; then
+    echo -e "\n${RED}[!] HLint found suggestion(s).${NC}"
+    exit $ERROR
+else
+    echo -e "\n${GREEN}[✓] No hints found.${NC}"
+    exit $SUCCESS
+fi
+
